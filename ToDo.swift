472264d4367bc1c9866fdde871f9bccac01b3e9b
Apple <apple@Apples-MacBook-Pro-79.local>
2019-08-12 11:11:22 -0400

    Complete Iteration 0

---
+++ ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  To-Do List
+//
+//  Created by Apple on 8/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+import UIKit
+
+class toDo{
+    var name = " "
+    var important = false
+}
